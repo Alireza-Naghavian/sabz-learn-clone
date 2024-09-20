f22db8dd6fb029b8588bcd3edfc25bd930681e6b
alireza <alirezanghngh1379@gmail.com>
2024-09-20 04:26:19 +0330

    Feat: -Set countDownTimer comp

--- src/components/layouts/course/CourseBanner.tsx
+++ src/components/layouts/course/CourseBanner.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function CourseBanner() {
   return (
-    <div className='course_intro_wrap order-1 w-full h-[360px] rounded-2xl overflow-hidden bg-darker'>
+    <div className='course_intro_wrap order-1 w-full  h-[360px] rounded-2xl overflow-hidden bg-darker'>
         {/* video player */}
     </div>
   )
