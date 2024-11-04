27b016790c9404a885cbfe4c67d9d0be806f1381
alireza <alirezanghngh1379@gmail.com>
2024-11-04 22:57:27 +0330

    Fix: revert auth gaurd for admin panel

--- src/app/my-account/page.tsx
+++ src/app/my-account/page.tsx
@@ -1,6 +1,7 @@
 import UserPanel from '@/components/layouts/user-panel/UserPanel';
 import { authUser } from '@/utils/auth/auth';
 import { Metadata } from 'next';
+import { redirect } from 'next/navigation';
 export const dynamic = "force-dynamic"
 export const metadata: Metadata = {
   applicationName: "سبز لرن",
@@ -15,8 +16,8 @@ export const metadata: Metadata = {
 };
 async function page() {
   const user = await authUser();
-  console.log(user);
-  // if (user === null|| user === undefined) return redirect("/");
+
+  if (user === null|| user === undefined) return redirect("/");
   return (
     <main className='md:bg-white md:dark:bg-gray-800 '><UserPanel/></main>
   )
